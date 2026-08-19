044698523471d3849a4fb23a00a0dbe7737f8039
RanggaRijasa <ranggarijasa2005@gmail.com>
2026-08-19 15:34:50 +0700

    first commit

---
+++ Cartsyne/CartsyneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartsyneApp.swift
+//  Cartsyne
+//
+//  Created by Rangga Rijasa on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CartsyneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
